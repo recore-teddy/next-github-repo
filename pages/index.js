cde4dabfab336e6f92b5bd50c35b78a1267fd5ab
Teddy Jung <teddy@reware.co.kr>
2021-07-03 20:04:19 +0900

    Loading UserInfo using Github API, isomorphic-unfetch

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const router = useRouter();
+  const [username, setUsername] = useState("");
   return (
     <div>
-      <div>
-        <img src="/teddy.jpeg" alt="profile-img" width="500px" />
-      </div>
-      <button type="button" onClick={() => router.push("/tomato")}>
-        tomato로 가기
-      </button>
-      <p>이름</p>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-        {name}으로 가기
-      </button>
+      <label>
+        Username
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <p>{username} 깃허브 검색하기</p>
+      <Link href={`users/${username}`}>
+        <a>검색하기</a>
+      </Link>
     </div>
   );
 };
---
+++ pages/users/[name].js
@@ -0,0 +1,31 @@
+import fetch from "isomorphic-unfetch";
+
+const name = ({ user }) => {
+  const username = user && user.name;
+  return (
+    <>
+      <div>{username}</div>
+      <div>
+        <img src={user.avatar_url} alt="user-avatar" />
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps = async ({ query }) => {
+  // console.log(query);
+  const { name } = query;
+  try {
+    const res = await fetch(`https://api.github.com/users/${name}`);
+    if (res.status === 200) {
+      const user = await res.json();
+      console.log(user);
+      return { props: { user } };
+    }
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
+};
+
+export default name;
