579e8d8eadd8fadd32122afe8c2e8b2eea993a00
Teddy Jung <teddy@reware.co.kr>
2021-07-04 19:48:59 +0900

    Github Repository Pagination

--- pages/users/[name].js
+++ pages/users/[name].js
@@ -1,112 +1,27 @@
 import css from "styled-jsx/css";
-import formatDistance from "date-fns/formatDistance";
+
 import Profile from "../../components/Profile";
-// import Repositories from "../../components/Repositories";
+import Repositories from "../../components/Repositories";
 
 const style = css`
   .user-content-wrapper {
     display: flex;
     padding: 20px;
   }
-  .repos-wrapper {
-    width: 100%;
-    height: 100vh;
-    overflow: scroll;
-    padding: 0px 16px;
-  }
-  .repos-header {
-    padding: 16px 0;
-    font-size: 14px;
-    font-weight: 600;
-    border-bottom: 1px sold #e1e4e8;
-  }
-  .repos-count {
-    display: inline-block;
-    padding: 2px 5px;
-    margin-left: 6px;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 1;
-    color: #586069;
-    background-color: rgba(27, 31, 35, 0.08);
-    border-radius: 20px;
-  }
-  .repository-wrapper {
-    width: 100%;
-    border-bottom: 1px solid #e1e4e8;
-    padding: 24px 0;
-  }
-
-  a {
-    text-decoration: none;
-  }
-  .repository-name {
-    margin: 0;
-    color: #0366d6;
-    font-size: 20px;
-    display: inline-block;
-    cursor: pointer;
-  }
-  .repository-name:hover {
-    text-decoration: underline;
-  }
-  .repository-description {
-    padding: 12px 0;
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-language {
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-updated-at {
-    margin-left: 20px;
-  }
 `;
 
 const name = ({ user, repos }) => {
-  if (!user) {
-    return null;
-  }
   return (
     <div className="user-content-wrapper">
       <Profile user={user} />
-      <div className="repos-wrapper">
-        <div className="repos-header">
-          Repositories
-          <span className="repos-count">{user.public_repos}</span>
-          {user &&
-            repos &&
-            repos.map((repo) => (
-              <div key={repo.id} className="repository-wrapper">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={`https://github.com/${user.login}/${repo.name}`}
-                >
-                  <h2 className="repository-name">{repo.name}</h2>
-                </a>
-                <p className="repository-description">{repo.description}</p>
-                <p className="respository-language">
-                  {!repo.language ? "No Langauge" : repo.language}
-                  <span className="repository-updated-at">
-                    {formatDistance(new Date(repo.updated_at), new Date(), {
-                      addSuffix: true,
-                    })}
-                  </span>
-                </p>
-              </div>
-            ))}
-        </div>
-      </div>
+      <Repositories user={user} repos={repos} />
       <style jsx>{style}</style>
     </div>
   );
 };
 
 export const getServerSideProps = async ({ query }) => {
-  // console.log(query);
-  const { name } = query;
+  const { name, page = 1 } = query;
   try {
     let user;
     let repos;
@@ -117,7 +32,7 @@ export const getServerSideProps = async ({ query }) => {
     }
 
     const repoRes = await fetch(
-      `https://api.github.com/users/${name}/repos?sort=updated&page=1&per_page=10`
+      `https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`
     );
     if (repoRes.status === 200) {
       repos = await repoRes.json();
